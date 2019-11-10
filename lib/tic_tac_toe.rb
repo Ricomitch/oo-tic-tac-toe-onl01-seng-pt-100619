b109104aa81ef0e2974b5040e2477364db1639aa
Rico Mitchell <ricomitch@icloud.com>
2019-11-10 05:33:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,11 @@
-require 'pry'
+ require 'pry'
 class TicTacToe
   
   def initialize
-    #sets the starting state of the board
-    #an array with 9 " " empty strings
-    #within an instance variable named @board
-    
     @board = Array.new(9, " ")
   end
   
   WIN_COMBINATIONS = [
-    
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -21,67 +16,36 @@ class TicTacToe
     [6,4,2]
     ]
     
-    
-    
-    def display_board
-      #prints the current board representation based on the @board instance variable
-      
+  def display_board
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
       puts "-----------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    end
-    
-    
-    def input_to_index(user_string)
-     #accepts the user's input (a string) as an argument
-     #converts the user's input (a string) into an integer
-     input = 0
-     input = user_string.to_i
-     
-      #converts the user's input from the user-friendly format (on a 1-9 scale) to the array-friendly format (where the first index starts at 0)
-      
-      input - 1
-    end
-    
-    
-    def move(index, token = "X")
-      #index: in the @board array that the player chose
-      #token: the players token, either "X" or "O"
-      
-      # index[token]
+  end 
+  
+  def input_to_index(user_string)
+      user_string.to_i - 1
+      #binding.pry
+  end
+  
+  def move (index, token = "X")
       @board[index] = token
-      
-    end
-    
-    
-    def position_taken?(position)
-      #it is passed the index value for a prospective move
-      #check if that position on the @board is vacant or if it contains X /O
-      #If the position is free, the method should return false (i.e., "the position is not taken"); otherwise, it will return true.
-      
-      #returns true/false based on whether the position on the board is already occupied
-      
+  end
+  
+  def position_taken?(position)
       if @board[position] == " " || @board[position] == nil
         false
       else
         true
       end
-    end
-    
-    
-    def valid_move?(position)
-      #returns true if the move is valid and false or nil if not
+  end
+  
+  def valid_move?(position)
     position.to_i.between?(0,8) && !position_taken?(position)
-      
-    end
-    
-    
-    def turn_count #counts occupied positions
-    #returns number of turns that have been played
-    #based on the board variable
-    
+  end
+  
+   def turn_count
     counter = 0
     @board.each do |token|
       if token == "X" || token == "O"
@@ -91,7 +55,6 @@ class TicTacToe
     counter
     end
     
-    
     def current_player
       if turn_count % 2 == 0
         "X"
@@ -100,42 +63,84 @@ class TicTacToe
       end
     end
     
-    
-    def turn
-    # receives user input via the gets method
-    # calls #input_to_index, #valid_move?, and #current_player
-    # makes valid moves and displays the board
-    # asks for input again after a failed validation
-    
-    
+  def turn
     puts "Please enter 1-9:"
     user_input = gets.strip
     index = input_to_index(user_input)
-    
-    #if index valid.. make a move for index.
-    #show the board
     if valid_move?(index)
       move(index, current_player)
       display_board
     else 
-    #ask for input again
       turn
     end
+  end
+  
+  def won?
+     WIN_COMBINATIONS.each {|win_combo|
+      index_0 = win_combo[0]
+      index_1 = win_combo[1]
+      index_2 = win_combo[2]
+  
+      position_1 = @board[index_0]
+      position_2 = @board[index_1]
+      position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
+    }
+    return false
+  end
+  
+  
+  def full?
+    @board.all? {|index| index == "X" || index == "O"}
+  end
+  
+  def draw?
+    if full? && !won?
+      true 
+    else
+      false
+    end
+  end
+  
+  def over?
+    if won? || full?
+      true
+    else
+      false
+    end
+  end
     
-    def won? 
-      is_won = false 
-      
-      WIN_COMBINATIONS.any? do |combo| 
-        # binding.pry
-        
-        if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
-          is_won = combo
-        elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
-          is_won = combo 
-        end
+    
+  def winner #?
+    index = []
+    index = won?
+    # binding.pry
+    if index == false
+      return nil
+    else
+      if @board[index[0]] == "X"
+        return "X"
+      else
+        return "O"
       end
-      is_won
     end
-    
-end 
+  end
+  
+  def play
+    until over? == true
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+  
+end  
